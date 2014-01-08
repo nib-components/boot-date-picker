e0092920ff07ebcc1068f64bd9bf298c28a56f21
chrisbuttery <info@chrisbuttery.com>
2014-01-08 16:35:12 +1100

    Created component

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
 boot-date-picker
 ================
 
-A boot component wrapping the functionality to trigger a date picker
+A boot component wrapping the functionality to trigger a date picker.
+
+`require('boot-date-picker');`
+
+## Installation
+
+    $ component install nib-components/boot-date-picker
+
+## API
+
+	`require('boot-date-picker');`
+
+
+## License
+
+  MIT
---
+++ index.js
@@ -0,0 +1,18 @@
+var DatePicker  = require('date-picker');
+var triggerEvent = require('trigger-event');
+
+/**
+ * Create datepickers
+ */
+var datepickers = document.querySelectorAll('[data-datepicker]') || [];
+
+Array.prototype.forEach.call(datepickers, function(el){
+  var picker = new DatePicker({
+    el: el,
+    position: el.getAttribute('data-datepicker-position'),
+    format: 'DDMMYYYY'
+  });
+  picker.on('select', function(val) {
+    triggerEvent(el, 'change');
+  });
+});
